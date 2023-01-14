e88228c6a7b739ae956ba657062bf023ab7c2b66
TUCCHI1 <tutikikin1@gmail.com>
2023-01-14 16:46:15 +0900

    add: first-slider-test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,39 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test("drag-and-drop test", async ({ page }) => {
+  // navigating to the Ant slider component test page
+  await page.goto("https://x2f9rh.csb.app/");
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+  // the value corresponding to the 100% of the slider
+  const maxValue = 20;
+  // drag-and-drop target value in parcentage
+  const targetValue = 0.4; // 40%
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+  // retrieving the slider handle HTML element
+  const sliderHandle = page.locator(".ant-slider-handle").first();
+  // retrieving the slider HTML element
+  const slider = page.locator(".ant-slider.ant-slider-horizontal").first();
 
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
+  // getting the slider bouding box size
+  const sliderBoundingBox = await slider.boundingBox();
 
-  // Expects the URL to contain intro.
-  await expect(page).toHaveURL(/.*intro/);
-});
+  // performing the drag-and-drop interaction
+  await sliderHandle.dragTo(sliderHandle, {
+    force: true,
+    targetPosition: {
+      // moving the slider to the target value in %
+      x: sliderBoundingBox ? sliderBoundingBox.width * targetValue : 0,
+      y: 0
+    }
+  })
+
+  // retrieving the input HTML element
+  const input = page.locator(".ant-input-number-input").first();
+  // getting the "value" HTML attribute
+  const value = await input.getAttribute("value");
+
+  // calculationg the expected value
+  const expectedValue = `${maxValue * targetValue}`;
+
+  expect(value).toEqual(expectedValue);
+})
